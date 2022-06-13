d622e2f542d85117ab1ed58b42f92e3dc7a1db2c
Trinidad Padilla <tpadilla101@live.com>
2022-06-12 19:23:43 -0700

    finished a progres sbar

--- src/components/progressBar/ProgressBar.jsx
+++ src/components/progressBar/ProgressBar.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function ProgressBar( { complete }) {
+function ProgressBar( /* { complete } */) {
 
     const [ style, setStyle ] = useState({});
+    const [ complete, setComplete ] = useState(10)
 
-    setTimeout( () => {
-        const newProgressStyles = {
-            opacity: 1,
-            width: `${complete}%`
-        }
 
-        setStyle(newProgressStyles);
-    }, 1000);
+    useEffect( () => {
+        setTimeout( () => {
+            const newProgressStyles = {
+                opacity: 1,
+                width: `${complete}%`,
+                maxWidth: '100%',
+            }
+
+            setStyle(newProgressStyles);
+        }, 100);
+    },[complete] )
+
+    const incrementBar = () => {
+
+        if(complete !== 100) {
+            setComplete(complete + 10)
+        };
+        
+    };
+    
 
     return (
 
@@ -19,6 +33,11 @@ function ProgressBar( { complete }) {
             <div className='progress-complete' style={style} >
                 {complete}%
             </div>
+
+            <button onClick={incrementBar}>
+
+                click
+            </button>
 
         </section>
 
