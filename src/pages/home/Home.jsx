bdd8eaaa05aee2b06461a541e17e771bcaf20d27
Trinidad Padilla <tpadilla101@live.com>
2023-03-01 10:26:15 -0800

    worked on a few ways to fetch data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     PokeCardList,
     ProgressBar,
@@ -11,6 +11,7 @@ const HomePage = () => {
     const [ pokeData, setPokeData ] = useState();
     const [ pokeQuery, setPokeQuery ] = useState('');
 
+/* Pokemon (endpoint): */
     const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
 
     const pokeQueryHandler = (event) => {
@@ -31,11 +32,75 @@ const HomePage = () => {
         event.preventDefault();
         fetchPokeData(pokeQuery);
         setPokeQuery('');
-    }
+    };
+
+/* useEffect handles initial fetch, and loading of data: */
+    /* useEffect(() => {
+        const savedPokeData = localStorage.getItem('pokeData');
+        if (savedPokeData) {
+            setPokeData([JSON.parse(savedPokeData)]);
+        } else {
+            fetch(`${BASE_URL}bulbasaur`)
+            .then(response => response.json()
+            .then(response => {
+                let initialData = JSON.stringify(response);
+                localStorage.setItem('pokeData', initialData);
+                setPokeData([response]);
+            }));
+        }
+        
+    }, []); */
+
+    /* useEffect(() => {
+        const savedPokeData = localStorage.getItem('pokeData');
+        if (savedPokeData) {
+            setPokeData([JSON.parse(savedPokeData)]);
+        } else {
+            fetch(`${BASE_URL}bulbasaur`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            .then(response => response.json()
+            .then(response => {
+                let initialData = JSON.stringify(response);
+                localStorage.setItem('pokeData', initialData);
+                setPokeData([response]);
+            }));
+        }
+        
+    }, []); */
+
+    useEffect(() => {
+        const fetchPokeData = async () => {
+          const savedPokeData = localStorage.getItem('pokeData');
+          if (savedPokeData) {
+            setPokeData([JSON.parse(savedPokeData)]);
+          } else {
+            try {
+              const response = await fetch(`${BASE_URL}bulbasaur`, {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                }
+              });
+              const data = await response.json();
+              let initialData = JSON.stringify(data);
+              localStorage.setItem('pokeData', initialData);
+              setPokeData([data]);
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        };
+      
+        fetchPokeData();
+      }, []);
 
     return (
         <main id="homepage-parent-container">
-            {/* <form className='searchbar-parent-container'>
+            <form className='searchbar-parent-container'>
                 <input
                     type='text'
                     id='searchbar'
@@ -52,21 +117,30 @@ const HomePage = () => {
                 >
                     Search
                 </button>                
-            </form> */}
+            </form>
 
            {/*  <ProgressBar 
                 complete="50"
             /> */}
 
-            <Form />
+            {/* <Form /> */}
 
         {/* PokeCardList displays a list of  PokeCards: */}
-           {/*  <PokeCardList 
+
+        {pokeData ? 
+        
+    
+            <PokeCardList 
                 pokeData={pokeData}
                 setPokeData={setPokeData}
                 pokeDataLocalStorage={pokeDataLocalStorage}
-            /> */}
-            
+            />
+
+            :
+
+            <p>No Pokemon Data!!!</p>
+        }
+        
 
         </main>
     );
